56ad0fdd969645ee83fbc46356722201dc9909e6
diegopessoa <diegopessoa160@gmail.com>
2025-04-10 22:50:32 -0400

    Entendo sobre métodos com parâmetros

---
+++ src/Metodos/test/CalculadoraTest.java
@@ -0,0 +1,15 @@
+package Metodos.test;
+
+import Metodos.dominio.Calculadora;
+
+public class CalculadoraTest {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumero();
+        System.out.println("---");
+        calculadora.subtraiDoisNumeros();
+        System.out.println("---");
+        // precisa de dois argumentos para executar
+        calculadora.multiplicaDoisNumeros(12,7);
+    }
+}
