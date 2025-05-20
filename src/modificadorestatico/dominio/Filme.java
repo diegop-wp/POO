aeace71b0b5dde71d64ee728a9d041599952f8ef
diegopessoa <diegopessoa160@gmail.com>
2025-05-19 23:11:27 -0400

    up

---
+++ src/modificadorestatico/dominio/Filme.java
@@ -0,0 +1,32 @@
+package modificadorestatico.dominio;
+
+public class Filme {
+    private String nome;
+    private int [] ep  = {1, 2, 3, 4, 5, 6, 7, 8};
+    {
+        System.out.println("dentro bloco de inicialização");
+        ep  = new int[100];
+        for (int i = 0; i < ep.length; i++) {
+            ep[i] = i+1;
+        }
+    }
+    public Filme(String nome) {
+        this.nome = nome;
+    }
+
+    public Filme() {
+
+        for (int eps : this.ep){
+            System.out.print(eps+" ");
+        }
+        System.out.println();
+    }
+
+    public int[] getEp() {
+        return ep;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
---
+++ src/modificadorestatico/teste/ProgramaFilme.java
@@ -0,0 +1,13 @@
+package modificadorestatico.teste;
+
+import modificadorestatico.dominio.Filme;
+
+public class ProgramaFilme  {
+    public static void main(String... args) {
+
+        Filme filme1 = new Filme();
+        Filme filme2 = new Filme();
+        Filme filme3 = new Filme();
+
+    }
+}
